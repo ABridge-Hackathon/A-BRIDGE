984f658cbb813b32fc3b1cd78df78e4b62d4c0df
Ubuntu <ubuntu@ip-172-31-32-43.ap-northeast-2.compute.internal>
2026-01-30 21:59:56 +0000

    aaa

--- backend/app/config/settings.py
+++ backend/app/config/settings.py
@@ -19,10 +19,10 @@ REDIS_DB = int(os.environ.get("REDIS_DB", "0"))
 SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "")
 DEBUG = os.environ.get("DJANGO_DEBUG", "0") == "1"
 ALLOWED_HOSTS = [
-    h.strip()
-    for h in os.environ.get("DJANGO_ALLOWED_HOSTS", "").split(",")
-    if h.strip()
-]
+    "localhost",
+    "127.0.0.1",
+    "unrustically-unpalatable-artie.ngrok-free.dev",
+    ]
 CORS_ALLOW_ALL_ORIGINS = True
 
 CORS_ALLOW_CREDENTIALS = True
